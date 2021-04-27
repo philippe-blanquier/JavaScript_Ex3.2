c3fea43155582db0a225f9ec13b283ba903ee456
philippe-blanquier <philippe.blanquier@gmail.com>
2021-04-27 10:39:38 +0200

    creation

---
+++ js_ex3.2.js
@@ -0,0 +1,11 @@
+// Java Script file: js_ex3.2.js
+
+let pId,textId;
+for (let numberIdx = 0; numberIdx <= 100; numberIdx++)
+{
+    if (numberIdx % 15 == 0)
+        console.log("...");
+    else
+        console.log(numberIdx);
+}
+
